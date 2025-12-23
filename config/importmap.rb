db352ddd8dca1bc2e4fea5d66e5e94c3e1b0099f
nicolas <nicolasaudicio95@gmail.com>
2025-12-23 17:46:08 -0300

    refactoring styles

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,3 +9,7 @@ pin "bootstrap" # @5.3.3
 pin "bootstrap-icons", to: "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css"
 pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
 pin "bootstrap", to: "bootstrap.min.js"
+pin "importmap" # @0.0.0
+pin "fs" # @2.1.0
+pin "process" # @2.1.0
+pin "util" # @2.1.0
